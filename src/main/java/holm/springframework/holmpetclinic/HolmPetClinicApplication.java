c17feb39ae9373f0d3a520a2f849a27c8587e648
Denis Kholmogorov <DKholmogorov@Bellintegrator.com>
2020-09-21 12:00:43 +0300

    Initial Project Add

---
+++ src/main/java/holm/springframework/holmpetclinic/HolmPetClinicApplication.java
@@ -0,0 +1,13 @@
+package holm.springframework.holmpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolmPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolmPetClinicApplication.class, args);
+    }
+
+}
